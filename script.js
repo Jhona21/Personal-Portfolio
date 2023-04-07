c873611129de246d6be94488af77ce54866384e8
Jhona21 <wyy21996@gmail.com>
2023-04-07 12:26:36 +0100

    Update to integrate a burguer menu, mobile view

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Get the button:
 let mybutton = document.getElementById("scroll-up");
+const burguer = document.getElementById("burguer");
+const openMenu = document.getElementById("open");
+const closeMenu = document.getElementById("close");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
@@ -17,3 +20,14 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+burguer.addEventListener("click", function(){
+    burguer.classList.toggle("open")
+    const burguerMenu = document.querySelectorAll(".nav-item-burguer");
+openMenu.classList.toggle("show-mobile");
+closeMenu.classList.toggle("hide-mobile")
+    console.log(burguerMenu)
+    burguerMenu.forEach((burguerMenu) => {
+        burguerMenu.classList.toggle("show")
+    })
+})
