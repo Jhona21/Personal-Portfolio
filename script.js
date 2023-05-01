2a7cb63aa7efae248e0a94fe929dad2c3fe2d56d
Jhona21 <wyy21996@gmail.com>
2023-05-01 18:00:40 +0100

    Added icon animation and fixed  to top button

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const closeMenu = document.getElementById("close");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 550) {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 850 & window.innerWidth > 780) {
         // adjuts the value above to decide when the scroll button displays
         mybutton.style.display = "block";
     } else {
@@ -30,4 +30,15 @@ closeMenu.classList.toggle("hide-mobile")
     burguerMenu.forEach((burguerMenu) => {
         burguerMenu.classList.toggle("show")
     })
+})
+// to stop the animation when hovered
+const icons = Array.from(document.querySelectorAll(".carrousel-icons"));
+
+icons.forEach(icon =>{
+    icon.addEventListener('mouseover', () => {
+        icon.style.animationPlayState = 'paused';
+    });
+    icon.addEventListener('mouseout', () => {
+        icon.style.animationPlayState = 'running';
+    });
 })
